c960b96b450d7489e9c0e0794adce3e0ffcc8926
RafaelPinto99 <pintorafael1999@gmail.com>
2024-02-02 14:57:59 +0000

    Updated Loops

--- Loops/main.js
+++ Loops/main.js
@@ -23,9 +23,29 @@
     i++
 }*/
 
-let name = "Rafael Pinto"
+/*let name = "Rafael Pinto"
 
 for (let i = 0; i <= name.length; i++){
 
     console.log(name.charAt(i));
+}*/
+
+let name = "Rafael";
+let counter = 0;
+let myLetter;
+
+while (counter <= 3) {
+    
+    myLetter = name[counter];
+    console.log(myLetter);
+
+    if (counter === 1) {
+        counter += 2;
+        continue; //goes back to the beginning
+    }
+
+    if (myLetter === "e") break;
+    counter++;
 }
+
+console.log(counter);
